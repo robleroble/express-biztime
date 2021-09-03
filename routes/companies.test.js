183c3c3bfce79b83ab7651a1044190b53ac1d4c4
Nick <nsand3@gmail.com>
2021-09-02 22:52:54 -0700

    finished tests for companies routes

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -28,3 +28,67 @@ describe("GET /companies", () => {
     expect(res.body).toEqual({ companies: [testCompany] });
   });
 });
+
+describe("GET /companies/:code", () => {
+  test("get a single company by code", async () => {
+    const res = await request(app).get("/companies/google");
+    expect(res.statusCode).toBe(200);
+    // we have to add an empty array for invoices because getting a single company expects invoices
+    testCompany.invoices = [];
+    expect(res.body).toEqual({ company: testCompany });
+  });
+  test("get error for using incorrect code", async () => {
+    const res = await request(app).get("/companies/apple");
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe("POST /companies", () => {
+  test("create a new company", async () => {
+    const res = await request(app)
+      .post("/companies")
+      .send({ name: "Apple", description: "apple is a company too" });
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toEqual({
+      company: {
+        code: "apple",
+        name: "Apple",
+        description: "apple is a company too",
+      },
+    });
+  });
+});
+
+describe("PUT /companies/google", () => {
+  test("edit a company", async () => {
+    const res = await request(app)
+      .put("/companies/google")
+      .send({ name: "goooooogle", description: "goooooogle is a companyyyyy" });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      company: {
+        code: "google",
+        name: "goooooogle",
+        description: "goooooogle is a companyyyyy",
+      },
+    });
+  });
+  test("try to edit a company that doesn't exist", async () => {
+    const res = await request(app)
+      .put("/companies/gooogle")
+      .send({ name: "yahoo", description: "yahoo is a company" });
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe("DELETE /companies/google", () => {
+  test("delete a company", async () => {
+    const res = await request(app).delete("/companies/google");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: "Deleted" });
+  });
+  test("delete a company that doesn't exist", async () => {
+    const res = await request(app).delete("/companies/apple");
+    expect(res.statusCode).toBe(404);
+  });
+});
