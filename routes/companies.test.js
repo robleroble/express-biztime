879e749e441e559016440ef1512ad5a7fdf0d9eb
Nick <nsand3@gmail.com>
2021-09-02 22:16:34 -0700

    began further study for assignment (tests)

---
+++ routes/companies.test.js
@@ -0,0 +1,30 @@
+// set up testmode
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db");
+
+let testCompany;
+beforeEach(async () => {
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description) VALUES ('google', 'Google', 'google is a company') RETURNING code, name, description`
+  );
+  testCompany = result.rows[0];
+});
+
+afterEach(async () => {
+  await db.query(`DELETE FROM companies`);
+});
+
+afterAll(async () => {
+  await db.end();
+});
+
+describe("GET /companies", () => {
+  test("get a list with one company", async () => {
+    const res = await request(app).get("/companies");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ companies: [testCompany] });
+  });
+});
